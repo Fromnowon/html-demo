676772b417c390d5b79993b78c1852f5afbc6639
Fromnowon <zihangzhao@gmail.com>
2024-04-25 13:20:01 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,11 @@
 $(window).load(function(){
     $('.preloader').fadeOut(1000); // set duration in brackets    
 });
-
+window.onbeforeunload = function(){
+          //刷新后页面自动回到顶部
+        document.documentElement.scrollTop = 0;  //ie下
+        document.body.scrollTop = 0;  //非ie
+ }
 
 /* Mobile Navigation
     -----------------------------------------------*/
